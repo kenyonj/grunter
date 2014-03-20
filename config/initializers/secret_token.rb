aa5621ce80ea49afa862e5fbe21b7a7e1fc16037
Justin Kenyon <kenyonj@gmail.com>
2014-03-19 23:53:54 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Grunter::Application.config.secret_key_base = 'a69d0fbb84bcfb9a717783b83d757ff59f776e29ba846a640b10619ecca5ed5eb425784e26e5028f70161a795b87ddb1ca2131688809b9b80c80d587aa50508e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Grunter::Application.config.session_store :cookie_store, key: '_grunter_session'
