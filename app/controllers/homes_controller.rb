0cbf4d3932aba8d6a249b28979c86a379c309f21
Justin Kenyon <kenyonj@gmail.com>
2014-03-23 20:51:37 -0400

    Update index page to login before showing grunts

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,8 +1,6 @@
 class HomesController < ApplicationController
-  before_action :authorize
   def show
     @grunt = Grunt.new
-    @grunts = current_user.grunt_feed
     session[:return_to] ||= request.referer
   end
 end
--- config/initializers/clearance.rb
+++ config/initializers/clearance.rb
@@ -1,3 +1,4 @@
 Clearance.configure do |config|
+  config.redirect_url = '/'
   config.mailer_sender = 'reply@example.com'
 end
